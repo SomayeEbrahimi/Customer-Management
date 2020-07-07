5a3e3219f85d4064c6bbcb5a0e25231418918af4
Somaye Ebrahimi <s.ebrahimi67@hotmail.com>
2020-07-07 21:50:59 +0430

    Remove sample codes

--- src/Server/CrmSolution.Server.Api/Controllers/CustomersController.cs
+++ src/Server/CrmSolution.Server.Api/Controllers/CustomersController.cs
@@ -1,5 +1,4 @@
-﻿using Bit.Core.Exceptions;
-using Bit.OData.ODataControllers;
+﻿using Bit.OData.ODataControllers;
 using CrmSolution.Server.Model;
 using CrmSolution.Shared.Dto;
 
@@ -7,13 +6,5 @@ namespace CrmSolution.Server.Api.Controllers
 {
     public class CustomersController : DtoSetController<CustomerDto, Customer, int>
     {
-        [Function]
-        public int Sum(int n1, int n2)
-        {
-            if (n1 < 0 || n2 < 0)
-                throw new BadRequestException(")-:");
-
-            return n1 + n2;
-        }
     }
 }
